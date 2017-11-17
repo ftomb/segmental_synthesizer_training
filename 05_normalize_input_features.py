198ea072392744c2ce391eb2e41e009812dfa357
ftomb <30320452+ftomb@users.noreply.github.com>
2017-11-17 11:25:58 +0100

    Update 05_normalize_input_features.py

--- 05_normalize_input_features.py
+++ 05_normalize_input_features.py
@@ -1,22 +1,34 @@
+from multiprocessing import Pool
 import numpy as np 
 import pickle
+import sys
 import os
 
+def normalize(title, mean_std_path, input_features_path, normalized_input_features_path):
 
-with open('mean_std/' + 'input_mean_std.pickle' , "rb") as f: 
-	input_mean, input_std = pickle.load(f)
+	with open(mean_std_path + 'input_mean_std.pickle' , "rb") as f: 
+		input_mean, input_std = pickle.load(f)
 
-input_titles = []
-for fn in os.listdir('input_features/'):
-	input_titles.append(fn)
-
-os.makedirs('normalized_input_features')
-for title in input_titles:
-
-	with open('input_features/' + title, "rb") as g: 
+	with open(input_features_path + title, "rb") as g: 
 		X = pickle.load(g)
 		X -= input_mean
-		X /= input_std
+		X /= input_std+np.finfo(float).eps
 
-	with open('normalized_input_features/' + title, "wb") as h:
+	with open(normalized_input_features_path + title, "wb") as h:
 		pickle.dump(X, h)
+
+
+if __name__ == '__main__':
+
+	mean_std_path = sys.argv[1]
+	input_features_path = sys.argv[2]
+	normalized_input_features_path = sys.argv[3]
+
+	input_titles = []
+	for fn in os.listdir(input_features_path):
+		basename, extension = os.path.splitext(fn)
+		if extension == '.pickle':
+			input_titles.append(fn)
+
+	p = Pool()
+	p.starmap(normalize, [(title, mean_std_path, input_features_path, normalized_input_features_path) for title in input_titles])
