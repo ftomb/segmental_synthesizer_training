2fbb74053f2619f651bf22d89ab8097d5adeb853
ftomb <30320452+ftomb@users.noreply.github.com>
2017-11-17 11:24:24 +0100

    Update 02_phone_dict.py

--- 02_phone_dict.py
+++ 02_phone_dict.py
@@ -1,31 +1,45 @@
 import numpy as np
 import pickle
 import tgt
+import sys
 import os
 
-titles = []
-for fn in os.listdir('wav_/'):
-	titles.append(fn[:-4])
+def generate_phone_set(titles, textgrid_path):
 
-phone_list = []
+	phone_list = []
 
-for title in titles:
+	for title in titles:
+		tgname = textgrid_path + title + '.TextGrid'
 
-	tgname = 'textgrid/' + title + '.TextGrid'
-	try:
 		tg = tgt.read_textgrid(tgname)
 		phones_tier = tg.get_tier_by_name('phones')
 
 		for phone in phones_tier:
 			phone_list.append(phone.text)
-	except:
-		pass
 
-ph_dict = {}
-for j, v in enumerate(set(phone_list)):
-	ph_dict[v] = np.eye(len(set(phone_list)), dtype=np.float64)[j]
+	return set(phone_list)
 
-os.makedirs('phone_dictionary')
+def generate_phone_dict(phone_set):
+	ph_dict = {}
+	for j, v in enumerate(phone_set):
+		ph_dict[v] = np.eye(len(phone_set), dtype=np.float64)[j]
+	return ph_dict
 
-with open('phone_dictionary/' + 'phone_dictionary' + ".dict", "wb") as f:
-	pickle.dump(ph_dict, f)
+
+if __name__ == '__main__':
+
+	textgrid_path = sys.argv[1]
+	phone_dict_path = sys.argv[2]
+
+	titles = []
+	for fn in os.listdir(textgrid_path):
+		basename, extension = os.path.splitext(fn)
+		if extension == '.TextGrid':
+			titles.append(basename)
+	
+	phone_set = generate_phone_set(titles, textgrid_path)
+	ph_dict = generate_phone_dict(phone_set)
+	
+	with open(phone_dict_path + 'phone_dictionary' + ".dict", "wb") as f:
+		pickle.dump(ph_dict, f)
+
