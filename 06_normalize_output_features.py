84633e0a3a067a00b6f6477c819856aa63a299b2
ftomb <30320452+ftomb@users.noreply.github.com>
2017-11-17 11:26:17 +0100

    Update 06_normalize_output_features.py

--- 06_normalize_output_features.py
+++ 06_normalize_output_features.py
@@ -1,21 +1,36 @@
+from multiprocessing import Pool
 import numpy as np 
 import pickle
+import sys
 import os
 
-with open('mean_std/' + 'output_mean_std.pickle' , "rb") as f: 
-	output_mean, output_std = pickle.load(f)
+def normalize(title, mean_std_path, output_features_path, normalized_output_features_path):
 
-output_titles = []
-for fn in os.listdir('output_features/'):
-	output_titles.append(fn)
+	with open(mean_std_path + 'output_mean_std.pickle' , "rb") as f: 
+		output_mean, output_std = pickle.load(f)
 
-os.makedirs('normalized_output_features')
-for title in output_titles:
-
-	with open('output_features/' + title, "rb") as g: 
+	with open(output_features_path + title, "rb") as g: 
 		X = pickle.load(g)
 		X -= output_mean
-		X /= output_std
+		X /= output_std+np.finfo(float).eps
 
-	with open('normalized_output_features/' + title, "wb") as h:
+	with open(normalized_output_features_path + title, "wb") as h:
 		pickle.dump(X, h)
+
+
+
+if __name__ == '__main__':
+
+	mean_std_path = sys.argv[1]
+	output_features_path = sys.argv[2]
+	normalized_output_features_path = sys.argv[3]
+
+
+	output_titles = []
+	for fn in os.listdir(output_features_path):
+		basename, extension = os.path.splitext(fn)
+		if extension == '.bap_mgc':
+			output_titles.append(fn)
+
+	p = Pool()
+	p.starmap(normalize, [(title, mean_std_path, output_features_path, normalized_output_features_path) for title in output_titles])
